cdbf9a242886c47d55f5c2984e8e142084ca69f1
John <johnjerickagapito@gmail.com>
2026-09-01 00:31:57 +0800

    Fix submit button and error handling

--- frontend/src/components/CategoryForm/CategoryForm.jsx
+++ frontend/src/components/CategoryForm/CategoryForm.jsx
@@ -1,15 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { assets } from '../../assets/assets';
+import { AppContext } from '../../context/AppContext';
+import { addCategory } from '../../service/CategoryService';
+import { toast } from 'react-hot-toast';
 
 
 const CategoryForm = () => {
-
-  //const [loading, setLoading] = useState(false);
+const {setCategories, categories} = useContext(AppContext);
+  const [loading, setLoading] = useState(false);
   const [image, setImage] = useState(false);
+
   const [data, setData] = useState({
     name: '',
     description: '',
-    bgColor: '#ffffff'
+    bgColor: '#2c2c2c'
   });
 
   const onChangeHandler = (e) => {
@@ -22,13 +26,44 @@ const CategoryForm = () => {
     console.log(data);
   }, [data]);
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    
+    if (!image) {
+            toast.error("Select image for category");
+            return;
+    }
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("category", JSON.stringify(data));
+    formData.append("file", image);
+    try {
+       const response = await addCategory(formData)
+       if (response.status === 201) {
+        setCategories([...categories, response.data]);
+        toast.success('Category added successfully');
+        setData({
+            name: '',
+            description: '',
+            bgColor: '#2c2c2c'
+        });
+        setImage(false);
+       }
+    }catch (error) {
+        console.error(error);
+        toast.error('Failed to add category');
+  }finally {
+    setLoading(false);
+  }
+  }
+
 
   return (
     <div className="mx-2 mt-2">
         <div className="row">
             <div className="card col-md-12 form-container">
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={onSubmitHandler}>
                         <div className="mb-3">
                             <label htmlFor="image" className="form-label"><img src = {image ? URL.createObjectURL(image) : assets.upload} alt = "" width = {48} ></img>
                                 <input type="file" name="image" id="image" className="form-control" hidden onChange={(e) => setImage(e.target.files[0])}/>
@@ -60,10 +95,11 @@ const CategoryForm = () => {
                             name="bgColor"
                             id="bgColor"
                             onChange={onChangeHandler}
-                            value={data.bgColor}
-                            placeholder="#ffffff"/>
+                            value={data.bgColor}/>
                         </div>
-                        <button type="submit" className="btn btn-primary w-100">Save</button>
+                        <button type="submit"
+                         disabled={loading}
+                         className="btn btn-primary w-100">{loading ? "Loading ... " : "Submit"}</button>
                     </form>
                 </div>
             </div>
